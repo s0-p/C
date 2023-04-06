e7e2eae034ebdd308a8deafe3095b5023b07b9ce
s0-p <b00v0429@gmail.com>
2023-04-06 18:24:39 +0900

    malloc example

--- Cproject1/CProject1/CProject1.cpp
+++ Cproject1/CProject1/CProject1.cpp
@@ -19,7 +19,25 @@
 
 int main()
 {
-	
+	int student;
+	int input;
+	int* score;
+	int sum = 0;
+	printf("학생의 수는? : ");
+	scanf("%d", &student);
+	score = (int*)malloc(student * sizeof(int));
+	for (int i = 0; i < student; i++)
+	{
+		printf("학생 %d의 점수: ", i);
+		scanf("%d", &input);
+		score[i] = input;
+	}
+	for (int i = 0; i < student; i++)
+	{
+		sum += score[i];
+	}
+	printf("전체 학생 평균 점수: %d", sum / student);
+	free(score);
 
 }
 
