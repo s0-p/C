7002c61fb2c657c8346449e0d3855e955f7fee34
s0-p <b00v0429@gmail.com>
2023-04-15 19:26:10 +0900

    sort items example

---
+++ SortItems/SortItems/SortItems.cpp
@@ -0,0 +1,115 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define ITEM_CNT 5
+
+typedef struct _ITEM
+{
+	char name[9];
+	int price;
+	float rating;
+}ITEM;
+
+int main()
+{
+	ITEM items[ITEM_CNT] = {
+		{"니콘", 1000000, 4.5f},
+		{"캐논", 1000354, 4.2f},
+		{"소니", 1234500, 3.8f},
+		{"라이카", 2134220, 4.0f},
+		{"삼성", 1347823, 5.0f}
+	};
+	int sortType;
+	int isTry = 1;
+
+	while (isTry)
+	{
+		printf("-------------------------------------------------------\n");
+		printf("번호\t\t상품명\t\t가격\t\t평점\n");
+		printf("-------------------------------------------------------\n");
+		for (int i = 0; i < ITEM_CNT; i++)
+		{
+			printf("[%2d]\t\t[%6s]\t", i + 1, items[i].name);
+			printf("[%d]\t[%.3f]\n", items[i].price, items[i].rating);
+		}
+		printf("-------------------------------------------------------\n");
+		printf("어떤 기준으로 정렬할지 숫자로 입력해주세요.\n");
+		printf("(1.이름순, 2.낮은 가격순, 3.높은 가격순, 4. 평점순, 0.종료) ");
+		scanf("%d", &sortType);
+		printf("\n");
+		switch (sortType)
+		{
+		case 0:
+			isTry = 0;
+			printf("프로그램을 종료합니다.\n");
+			break;
+		case 1:
+			printf("이름순\n");
+			for (int i = 0; i < ITEM_CNT - 1; i++)
+			{
+				for (int j = 0; j < ITEM_CNT - i - 1; j++)
+				{
+					if (strcmp(items[j].name, items[j + 1].name) == 1)
+					{
+						ITEM tmp = items[j];
+						items[j] = items[j + 1];
+						items[j + 1] = tmp;
+					}
+				}
+			}
+			break;
+		case 2:
+			printf("낮은 가격순\n");
+			for (int i = 0; i < ITEM_CNT - 1; i++)
+			{
+				for (int j = 0; j < ITEM_CNT - i - 1; j++)
+				{
+					if (items[j].price > items[j + 1].price)
+					{
+						ITEM tmp = items[j];
+						items[j] = items[j + 1];
+						items[j + 1] = tmp;
+					}
+				}
+			}
+			break;
+		case 3:
+			printf("높은 가격순\n");
+			for (int i = 0; i < ITEM_CNT - 1; i++)
+			{
+				for (int j = 0; j < ITEM_CNT - i - 1; j++)
+				{
+					if (items[j].price < items[j + 1].price)
+					{
+						ITEM tmp = items[j];
+						items[j] = items[j + 1];
+						items[j + 1] = tmp;
+					}
+				}
+			}
+			break;
+		case 4:
+			printf("평점순\n");
+			for (int i = 0; i < ITEM_CNT - 1; i++)
+			{
+				for (int j = 0; j < ITEM_CNT - i - 1; j++)
+				{
+					if (items[j].rating < items[j + 1].rating)
+					{
+						ITEM tmp = items[j];
+						items[j] = items[j + 1];
+						items[j + 1] = tmp;
+					}
+				}
+			}
+			break;
+		default:
+			printf("잘못된 입력입니다.\n");
+			break;
+		}
+
+
+
+
+	}
+}
